f6c0b46b5e397cf6aed9da6e01c8ee277753b4c9
Jeremy Morrell <jmorrell@heroku.com>
2017-04-05 13:53:18 -0600

    tweaks

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var app = express();
 var port = process.env.PORT || 5000;
 app.get('/', (req, res) => res.send('Hello World!'));
 
-// Wait a full second before logging so that we can be sure
+// Wait a full 2 seconds before logging so that we can be sure
 // that pm2 is listening and not swallowing logs
 setTimeout(() => {
   console.log(Math.random());
   app.listen(port, () => console.log(`Listening on port: ${port}!`));
-}, 1000);
+}, 2000);
